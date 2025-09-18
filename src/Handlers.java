e257bc66c4155cc409a223cd7bd09ddd8a7c8977
dimiskent <dimitrios@skenteridis.be>
2025-09-18 12:14:50 +0200

    new files

--- src/Handlers.java
+++ src/Handlers.java
@@ -1,24 +1,33 @@
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import jdk.jfr.ContentType;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
 import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
 
 public class Handlers implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         Response response = getContents(exchange.getRequestURI().toString());
         String reply = response.response;
+        Headers responseHeaders = exchange.getResponseHeaders();
+        responseHeaders.set("Content-Type", response.contentType);
         exchange.sendResponseHeaders(response.code, reply.length());
         OutputStream stream = exchange.getResponseBody();
         stream.write(reply.getBytes());
         stream.close();
     }
     private Response getContents(String filePath) {
-        Response res = new Response(200, "");
+        Response res = new Response(200, "", "text/plain");
         Path path = FileSystems.getDefault().getPath(System.getProperty("user.dir"), "pub", filePath);
         File myFile = new File(path.toUri());
         if(!myFile.exists()) {
@@ -27,12 +36,16 @@ public class Handlers implements HttpHandler {
             return res;
         }
         try {
+            res.contentType = Files.probeContentType(path);
             Scanner scan = new Scanner(myFile);
-            res.response = scan.nextLine();
+            while (scan.hasNextLine()) {
+                res.response += scan.nextLine() + "\n";
+            }
             scan.close();
             return res;
         } catch (Exception e) {
             // todo handle 500 for java eval
+            System.out.println(e.getMessage());
             String errorType = e.getClass().toString().split(" ")[1];
             switch (errorType) {
                 case "java.io.FileNotFoundException":
--- src/Response.java
+++ src/Response.java
@@ -1,8 +1,10 @@
 public class Response {
     public int code;
     public String response;
-    public Response(int code, String response) {
+    public String contentType;
+    public Response(int code, String response, String contentType) {
         this.code = code;
         this.response = response;
+        this.contentType = contentType;
     }
 }
